e6d23612b269d1f068967c25404cb1e3b3f08ef6
Jaryd <jarydchandos.5@gmail.com>
2019-04-23 11:13:17 -0700

    J

--- pymongodb_test.py
+++ pymongodb_test.py
@@ -2,22 +2,24 @@ import pymongo
 import os
 import sys
 import pprint
+import dns
 
 def main():
-    url = 'mongodb://{}:{}@{}:{}/{}'.format(
+    url = 'mongodb+srv://{}:{}@{}/{}'.format(
         os.environ["MONGO_USERNAME"],
         os.environ["MONGO_PASSWORD"],
         os.environ["MONGO_HOST"],
-        os.environ["MONGO_DBNAME"])
+        os.environ["MONGO_DBNAME"]
+    )
         #mongodb+srv://Objective:<password>@cluster0-emrl4.mongodb.net/test?retryWrites=true
     client = pymongo.MongoClient(url)
     db = client[os.environ["MONGO_DBNAME"]]
     collection = db['Finances'] #put the name of your collection in the quotes
 
     #1. print the number of documents in collection
-    posts.count_documetns({})
+    print(collection.count_documents({}))
     #2. print the first document in the collection
-    posts.count_documents({"Name":"Jaryd"})
+    print(collection.count_documents({}))
     #3. print all documents in the collection
 
     #4. print all documents with a particular value for some attribute
